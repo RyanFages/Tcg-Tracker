825f4c3bfe148d4a62583f51e31d803d756d15bb
RyanFages <114567771+RyanFages@users.noreply.github.com>
2026-02-05 22:16:37 +0100

    api key supp

--- OP-Tracker/src/config/firebase.ts
+++ OP-Tracker/src/config/firebase.ts
@@ -10,13 +10,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAcXMkoMCTuddbdW5TNWOYaHb0BF8yZJ_U",
-  authDomain: "op-tracker-db.firebaseapp.com",
-  projectId: "op-tracker-db",
-  storageBucket: "op-tracker-db.firebasestorage.app",
-  messagingSenderId: "1054414206870",
-  appId: "1:1054414206870:web:1406f131a9b2aac6214fb7",
-  measurementId: "G-Y4F9EVETM6"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MESSUREMENT_ID
 };
 
 // Initialize Firebase
